75052fdd6c254c5adbf1b8ad079eac90d310cc9b
Aanya Hudda <aanya_hudda@brown.edu>
2024-04-01 19:37:24 -0400

    sorting is working?

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,24 +10,50 @@ bookData.forEach((item) => {
 });
 
 function App() {
+  // Total Number of Books in Want To Read
   const [wantToReadTotal, setWantToReadTotal] = useState(0);
   const incrementWantToReadTotal = () => setWantToReadTotal(wantToReadTotal + 1);
   const decrementWantToReadTotal = () => setWantToReadTotal(wantToReadTotal - 1);
 
+  // Books in Want To Read
   const [wantToReadBooks, setWantToReadBooks] = useState(new Set());
   const addToWantToReadBooks = (newBook) => setWantToReadBooks(new Set([... wantToReadBooks, newBook]))
   const removeFromWantToReadBooks = (bookToRemove) => setWantToReadBooks(new Set([... wantToReadBooks].filter((book) => book !== bookToRemove)));
+
+  // Sorting Books
+  const [sortedBooks, setSortedBooks] = useState(bookData);
+
+  // Sort Books by Descending Page Length
+  const sortByLength = () => {
+    const sorted = [...bookData].sort((a, b) => a.length - b.length);
+    setSortedBooks(sorted);
+  };
+
+  // Reset Sorting Back To Default
+  const resetSorting = () => {
+    setSortedBooks(bookData);
+  };
 
   return (
     <div className="App">
       <header id="Library">
         <h1>Library</h1>
-        {bookData.map((item, index) => (
-        <BookItem key={index} title={item.title} author={item.author} genre={item.genre} stars={item.stars} length={item.length} image={item.image} 
-           incrementWantToReadTotal={incrementWantToReadTotal} 
-           decrementWantToReadTotal = {decrementWantToReadTotal} 
-           addToWantToReadBooks={(newBook) => addToWantToReadBooks(newBook)} 
-           removeFromWantToReadBooks={(bookToRemove) => removeFromWantToReadBooks(bookToRemove)}/>
+        <button onClick={sortByLength}>Sort By Shortest to Longest Book Length</button>
+        <button onClick={resetSorting}>Reset</button>
+        {(sortedBooks).map((item, index) => (
+          <BookItem
+            key={index}
+            title={item.title}
+            author={item.author}
+            genre={item.genre}
+            stars={item.stars}
+            length={item.length}
+            image={item.image}
+            incrementWantToReadTotal={incrementWantToReadTotal}
+            decrementWantToReadTotal={decrementWantToReadTotal}
+            addToWantToReadBooks={(newBook) => addToWantToReadBooks(newBook)}
+            removeFromWantToReadBooks={(bookToRemove) => removeFromWantToReadBooks(bookToRemove)}
+          />
         ))}
       </header>
       <header id="Filters">
