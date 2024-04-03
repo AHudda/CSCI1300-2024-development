7ff3f6298e76cfe549c72c14d7dbd723cd7b5135
Aanya Hudda <aanya_hudda@brown.edu>
2024-04-03 17:59:19 -0400

    added another aria label, now done

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props){
     return (
-        <button onClick={props.onClick}>
+        <button onClick={props.onClick} aria-label="button to add or remove book from want to read list">
             {props.text}
         </button>
     )
